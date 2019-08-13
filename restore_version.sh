00a40782c5430f56dffd77fe83c4ffaef679cff4
T. Cameron Waller <tcameronwaller@gmail.com>
2019-08-12 22:12:58 -0700

    added new version indicator

--- restore_version.sh
+++ restore_version.sh
@@ -21,13 +21,25 @@ echo "access current version of the program..."
 rm master.zip
 wget https://github.com/tcameronwaller/metabonet/archive/master.zip
 unzip master.zip
+sudo chmod -R 777 ~/Downloads/metabonet-master/
 cd metabonet-master/
 ls ./
-sudo python3 setup.py install
+
+echo "................................................."
+echo "................................................."
+echo "................................................."
+echo "................................................."
+echo "................................................."
+echo "uninstall previous version..."
+python3 setup.py install --user --record installation_files.txt
+cat installation_files.txt | sudo xargs rm -rf
+rm -rf installation_files.txt
+
+python3 setup.py install --user --force --prefix=
 cd ~/Downloads/
 rm master.zip
-#chmod -R 777 metabonet-master/
-#rm -rf metabonet-master/
+chmod -R 777 ~/Downloads/metabonet-master/
+rm -rf metabonet-master/
 
 cd ~
 metabonet --help
